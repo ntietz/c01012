7a4c7018f52d71207d2044b9960b99287f79aad6
Nicholas Tietz <ntietz@gmail.com>
2017-01-28 18:38:11 -0500

    Styling like a pro. Pretty sure I'm a hotshot web developer now, since this is *so* pretty.

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,49 @@
 
+function oppositeColor(hex) {
+  var shortCode = false;
+  if (hex.length == 3) {
+    shortCode = true;
+    hex = hex.substring(0,1).repeat(2) + hex.substring(1,2).repeat(2) + hex.substring(2,3).repeat(2);
+  }
+  var r = parseInt(hex.substring(0,2), 16);
+  var g = parseInt(hex.substring(2,4), 16);
+  var b = parseInt(hex.substring(4,6), 16);
+
+  var r1 = (r + 128) % 255;
+  var g1 = (g + 128) % 255;
+  var b1 = (b + 128) % 255;
+
+  var opposite = r1.toString(16) + g1.toString(16) + b1.toString(16);
+
+  if (shortCode) {
+    opposite  = opposite.substring(0,1) + opposite.substring(2,3) + opposite.substring(4,5);
+  }
+
+  return opposite;
+}
+
 function addColorSectionsToPage(elem) {
   for (let each of words) {
     var colorCode = "#" + each.color;
-    var textColor = "#" + (0xFFFFFF ^ parseInt(each.color, 16)).toString(16);
+    //var textColor = "#" + (~ parseInt(each.color, 16)).toString(16);
+    var textColor = "#" + oppositeColor(each.color);
 
     var anchor = document.createElement("a");
     anchor.setAttribute("name", each.color);
     elem.appendChild(anchor);
 
+    var row = document.createElement("div");
+    row.className = "row";
+    elem.appendChild(row);
+
+    var col = document.createElement("div");
+    col.className="col-md-6 col-md-offset-3";
+    row.appendChild(col);
+
     var jumbotron = document.createElement("div");
-    jumbotron.className = "jumbotron";
+    jumbotron.className = "jumbotron rounded";
     jumbotron.style.backgroundColor = colorCode;
-    elem.appendChild(jumbotron);
+    col.appendChild(jumbotron);
 
     var container = document.createElement("div");
     container.className = "container";
